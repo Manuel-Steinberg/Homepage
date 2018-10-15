fe283a11039e41bf9d8b9652b50a87eb637e4c6c
Manuel Steinberg <github@manuel-steinberg.de>
2018-10-15 20:44:55 +0200

    implement new version

--- site/snippets/i-am.php
+++
@@ -1,9 +0,0 @@
-<form class="pure-form pure-form-stacke">
-    <div class="pure-g">
-        <div class="pure-u-1 pure-u-md-1-3"></div>
-        <div class="pure-u-1 pure-u-md-2-3">
-            <input type="hidden" name="csrf" value="<?php echo csrf() ?>">
-            <input id="i-am" class="pure-u-1" type="text" readonly>
-        </div>
-    </div>
-</form>
